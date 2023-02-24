1b05a0d79870b5f0f6ecabec8b0da092335228f4
ShanDewage <shandias07@gmail.com>
2023-02-24 23:55:58 +0530

    create navbar responsive

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -11,13 +11,15 @@ export default function App() {
    
   <div  className="bg-img">
     <div className='hero-container'>
-      <h3>Sit sunt consequatur nam facilis odit est nobis galisum.</h3>
-    <h1>Lorem ipsum dolor sit amet.</h1>
+      <h3>Lorem ipsum dolor sit amet</h3>
+    <h1>Sit sunt consequatur nam facilis odit est nobis galisum.</h1>
     <p>Lorem ipsum dolor sit amet. Est nesciunt unde et rerum obcaecati
        a rerum voluptatem 33 exercitationem adipisci est Quis recusandae
         ad iste quisquam eum voluptate fugiat! Ea molestiae magnam est
          laudantium neque et laborum tempore ut sint rerum aut odit eligendi.
       </p>
+
+      <button className='btn-shop'>Shop</button>
     </div>
   
   </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,19 +3,46 @@ import '../components/Navbar.css';
 //import { Navbar, Button,Text, useTheme } from "@nextui-org/react";
 import { Navbar, useTheme } from "@nextui-org/react";
 
+import React, { useState } from 'react';
+
 export default function App() {
   const { isDark } = useTheme();
- 
+
+  const [click, setClick] = useState(false);
+  //const [button, setButton] = useState(true);
+
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
+ /* const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
+
+  useEffect(() => {
+    showButton();
+  }, []);
+
+  window.addEventListener('resize', showButton);
+  */
 
   return (
   <>  
         <Navbar className="navbar-container" shouldHideOnScroll  isBordered={isDark} variant="sticky"  >
         <Navbar.Brand >
          {/* <Text b color="inherit" hideIn="xs">Cash & Carry </Text>*/}
-          <Link to='/' className="nav-logo">Cash & Carry</Link>
+          <Link to='/' className="nav-logo" onClick={closeMobileMenu}>
+          <i class="fa-sharp fa-solid fa-feather-pointed"></i>
+            Cash & Carry</Link>
         </Navbar.Brand>
+        <div className='menu-icon' onClick={handleClick}>
+            <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+          </div>
 
-        <Navbar.Content hideIn="xs"  className="nav-content">
+        <Navbar.Content hideIn="xs"  className={click ? 'nav-menu active' : 'nav-menu'}>
         { /* <Navbar.Link isActive href="/">Home</Navbar.Link>
           <Navbar.Link  href="#">Promotions</Navbar.Link>
           <Navbar.Link  href="#">Recipes</Navbar.Link>
@@ -24,13 +51,14 @@ export default function App() {
           <Navbar.Link  href="#">News</Navbar.Link>
           <Navbar.Link  href="about">About</Navbar.Link>*/}
 
-          <Link to='/' className='nav-links'>Home</Link>
-          <Link to='#' className='nav-links'>Promotions</Link>
-          <Link to='#' className='nav-links'>Delivery Saver</Link>
-          <Link to='#' className='nav-links'>Recipes</Link>
-          <Link to='#' className='nav-links'>Stores</Link>
-          <Link to='#' className='nav-links'>News</Link>
-          <Link to='about' className='nav-links'>About</Link>
+          <Link to='/' className='nav-links'  onClick={closeMobileMenu}>Home</Link>
+          <Link to='#' className='nav-links'  onClick={closeMobileMenu}>Promotions</Link>
+          <Link to='#' className='nav-links'  onClick={closeMobileMenu}>Delivery Saver</Link>
+          <Link to='#' className='nav-links'  onClick={closeMobileMenu}>Recipes</Link>
+          <Link to='#' className='nav-links'  onClick={closeMobileMenu}>Stores</Link>
+          <Link to='#' className='nav-links'  onClick={closeMobileMenu}>News</Link>
+          <Link to='/about' className='nav-links'  onClick={closeMobileMenu}>About</Link>
+          
         </Navbar.Content>
         <Navbar.Content>
           {/*
@@ -44,7 +72,7 @@ export default function App() {
           </Navbar.Item>
           */}
 
-          <Link to='#' className='btn-signup'>Sign up</Link>
+          <Link to='#' className='btn-signup nav-links-mobile'  onClick={closeMobileMenu}>Sign up</Link>
           
         </Navbar.Content>
       </Navbar>
